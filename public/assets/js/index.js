1fa7b53df8630eb12d56abf580e7fc21a86b028a
Jair Gomez <jairherber505@gmail.com>
2023-06-05 15:37:17 -0700

    meh

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -100,6 +100,10 @@ const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
+
+  if (click) {
+    
+  }
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
